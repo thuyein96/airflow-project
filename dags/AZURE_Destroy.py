726d31440608608ab8ec10e79a0a8e3a2f0f1cf7
Thu Yein <thuyein1996alpha@gmail.com>
2025-12-03 14:43:52 +0700

    reverse to success azure destroy

--- dags/AZURE_Destroy.py
+++ dags/AZURE_Destroy.py
@@ -4,7 +4,7 @@ import pika
 import psycopg2
 import shutil
 from airflow import DAG
-from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.operators.python import PythonOperator,BranchPythonOperator
 from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
@@ -84,97 +84,7 @@ def repository_name(request_id):
         connection.close()
 
 # -------------------------
-# Step 3: Branch based on resources
-# -------------------------
-def branch_resources(request_id):
-    load_dotenv(expanduser('/opt/airflow/dags/.env'))
-
-    USER = os.getenv("DB_USER")
-    PASSWORD = os.getenv("DB_PASSWORD")
-    HOST = os.getenv("DB_HOST")
-    PORT = os.getenv("DB_PORT")
-    DBNAME = os.getenv("DB_NAME")
-
-    connection = psycopg2.connect(
-        user=USER,
-        password=PASSWORD,
-        host=HOST,
-        port=PORT,
-        dbname=DBNAME,
-    )
-    cursor = connection.cursor()
-    try:
-        cursor.execute('SELECT "resourcesId" FROM "Request" WHERE id = %s;', (request_id,))
-        res = cursor.fetchone()
-        if not res:
-            raise ValueError(f"No request found for id={request_id}")
-        resourcesId = res[0]
-
-        cursor.execute(
-            'SELECT "resourceConfigId" FROM "Resources" WHERE id = %s;',
-            (resourcesId,)
-        )
-        resource = cursor.fetchone()
-        if not resource:
-            raise ValueError(f"No resource found for resourcesId={resourcesId}")
-
-        resourceConfigId = resource[0]
-        
-        # Count Azure VM instances
-        cursor.execute(
-            'SELECT id FROM "AzureVMInstance" WHERE "resourceConfigId" = %s;',
-            (resourceConfigId,)
-        )
-        vm_instances = cursor.fetchall()
-        vm_count = len(vm_instances)
-
-        # Count Azure DB instances
-        cursor.execute(
-            'SELECT id FROM "AzureDatabaseInstance" WHERE "resourceConfigId" = %s;',
-            (resourceConfigId,)
-        )
-        db_instances = cursor.fetchall()
-        db_count = len(db_instances)
-
-        # Count Azure Storage instances
-        cursor.execute(
-            'SELECT id FROM "AzureStorageInstance" WHERE "resourceConfigId" = %s;',
-            (resourceConfigId,)
-        )
-        st_instances = cursor.fetchall()
-        st_count = len(st_instances)
-
-        # Count Azure K8s clusters
-        cursor.execute(
-            'SELECT id FROM "AzureK8sCluster" WHERE "resourceConfigId" = %s;',
-            (resourceConfigId,)
-        )
-        k8s_instances = cursor.fetchall()
-        k8s_count = len(k8s_instances)
-
-        branches = []
-        if vm_count > 0:
-            branches.append('terraform_destroy_vm')
-        if db_count > 0:
-            branches.append('terraform_destroy_db')
-        if st_count > 0:
-            branches.append('terraform_destroy_st')
-        if k8s_count > 0:
-            branches.append('terraform_destroy_k8s')
-        
-        # Always destroy resource group last
-        # branches.append('terraform_destroy_rg')
-        
-        if not branches:
-            return 'end'
-        
-        return branches
-    finally:
-        cursor.close()
-        connection.close()
-
-# -------------------------
-# Step 4: Cleanup folder
+# Step 3: Cleanup folder
 # -------------------------
 def cleanup_directory(projectName):
     directory_path = f"/opt/airflow/dags/terraform/{projectName}"
@@ -182,9 +92,6 @@ def cleanup_directory(projectName):
         shutil.rmtree(directory_path)
     return projectName
 
-# -------------------------
-# Step 5: Delete from Database
-# -------------------------
 def supabase_delete_request(request_id):
     load_dotenv(expanduser('/opt/airflow/dags/.env'))
 
@@ -220,59 +127,52 @@ def supabase_delete_request(request_id):
         connection.commit()
         print(f"Deleted repository with id={repositoryId}")
 
-        # GET resourceConfigId
+        # GET resourceConfigId, CloudProvider
         cursor.execute('SELECT "resourceConfigId" FROM "Resources" WHERE id = %s;', (resourcesId,))
         res = cursor.fetchone()
         if not res:
             raise ValueError(f"No resources found for id={resourcesId}")
-        resourceConfigId = res[0]
+        resourceConfigId = res
 
-        # Delete Azure VM Instance
-        cursor.execute('SELECT * FROM "AzureVMInstance" WHERE "resourceConfigId" = %s;', (resourceConfigId,))
-        res = cursor.fetchall()
-        if res:
-            cursor.execute('DELETE FROM "AzureVMInstance" WHERE "resourceConfigId" = %s;', (resourceConfigId,))
-            connection.commit()
-            print(f"Deleted AzureVMInstance with resourceConfigId={resourceConfigId}")
-
-        # Delete Azure Database Instance
-        cursor.execute('SELECT * FROM "AzureDatabaseInstance" WHERE "resourceConfigId" = %s;', (resourceConfigId,))
-        res = cursor.fetchall()
-        if res:
-            cursor.execute('DELETE FROM "AzureDatabaseInstance" WHERE "resourceConfigId" = %s;', (resourceConfigId,))
-            connection.commit()
-            print(f"Deleted AzureDatabaseInstance with resourceConfigId={resourceConfigId}")
-    
-        # Delete Azure Storage Instance
-        cursor.execute('SELECT * FROM "AzureStorageInstance" WHERE "resourceConfigId" = %s;', (resourceConfigId,))
-        res = cursor.fetchall()
-        if res:
-            cursor.execute('DELETE FROM "AzureStorageInstance" WHERE "resourceConfigId" = %s;', (resourceConfigId,))
-            connection.commit()
-            print(f"Deleted AzureStorageInstance with resourceConfigId={resourceConfigId}")
-
-        # Delete Azure K8s Cluster
-        cursor.execute('SELECT * FROM "AzureK8sCluster" WHERE "resourceConfigId" = %s;', (resourceConfigId,))
-        res = cursor.fetchall()
-        if res:
-            cursor.execute('DELETE FROM "AzureK8sCluster" WHERE "resourceConfigId" = %s;', (resourceConfigId,))
-            connection.commit()
-            print(f"Deleted AzureK8sCluster with resourceConfigId={resourceConfigId}")
-
-        # Delete Azure Resource Group
-        # cursor.execute('SELECT * FROM "AzureResourceGroup" WHERE "resourceConfigId" = %s;', (resourceConfigId,))
-        # res = cursor.fetchall()
-        # if res:
-        #     cursor.execute('DELETE FROM "AzureResourceGroup" WHERE "resourceConfigId" = %s;', (resourceConfigId,))
-        #     connection.commit()
-        #     print(f"Deleted AzureResourceGroup with resourceConfigId={resourceConfigId}")
-
-        # Delete Resources
+        # Delete resources record
         cursor.execute('DELETE FROM "Resources" WHERE id = %s;', (resourcesId,))
         connection.commit()
-        print(f"Deleted Resources with id={resourcesId}")
+        print(f"Deleted resources with id={resourcesId}")
+
+        # Delete resources
+        # AWS VM Instance
+        cursor.execute('SELECT * FROM "AwsVMInstance" WHERE "resourceConfigId" = %s;', (resourceConfigId,))
+        res = cursor.fetchall()
+        if res:
+            cursor.execute('DELETE FROM "AwsVMInstance" WHERE "resourceConfigId" = %s;', (resourceConfigId,))
+            connection.commit()
+            print(f"Deleted AwsVMInstance with id={resourceConfigId}")
+
+        # AWS Database Instance
+        cursor.execute('SELECT * FROM "AwsDatabaseInstance" WHERE "resourceConfigId" = %s;', (resourceConfigId,))
+        res = cursor.fetchall()
+        if res:
+            cursor.execute('DELETE FROM "AwsDatabaseInstance" WHERE "resourceConfigId" = %s;', (resourceConfigId,))
+            connection.commit()
+            print(f"Deleted AwsDatabaseInstance with id={resourceConfigId}")
+    
+        # AWS Storage Instance
+        cursor.execute('SELECT * FROM "AwsStorageInstance" WHERE "resourceConfigId" = %s;', (resourceConfigId,))
+        res = cursor.fetchall()
+        if res:
+            cursor.execute('DELETE FROM "AwsStorageInstance" WHERE "resourceConfigId" = %s;', (resourceConfigId,))
+            connection.commit()
+            print(f"Deleted AwsStorageInstance with id={resourceConfigId}")
+
+        # Azure Resource Group Instance
+        cursor.execute('SELECT * FROM "Resources" WHERE "resourceConfigId" = %s;', (resourceConfigId,))
+        res = cursor.fetchall()
+        if res:
+            cursor.execute('DELETE FROM "Resources" WHERE "resourceConfigId" = %s;', (resourceConfigId,))
+            connection.commit()
+            print(f"Deleted Resources with id={resourceConfigId}")
         
-        # Delete Resource Config
+        # Resource Config
         cursor.execute('DELETE FROM "ResourceConfig" WHERE id = %s;', (resourceConfigId,))
         connection.commit()
         print(f"Deleted ResourceConfig with id={resourceConfigId}")
@@ -281,11 +181,85 @@ def supabase_delete_request(request_id):
         cursor.close()
         connection.close()
 
+def branch_resources(request_id):
+    load_dotenv(expanduser('/opt/airflow/dags/.env'))
+
+    USER = os.getenv("DB_USER")
+    PASSWORD = os.getenv("DB_PASSWORD")
+    HOST = os.getenv("DB_HOST")
+    PORT = os.getenv("DB_PORT")
+    DBNAME = os.getenv("DB_NAME")
+
+    connection = psycopg2.connect(
+        user=USER,
+        password=PASSWORD,
+        host=HOST,
+        port=PORT,
+        dbname=DBNAME,
+    )
+    cursor = connection.cursor()
+    try:
+        cursor.execute('SELECT "resourcesId" FROM "Request" WHERE id = %s;', (request_id,))
+        res = cursor.fetchone()
+        if not res:
+            raise ValueError(f"No request found for id={request_id}")
+        resourcesId = res[0]
+
+        cursor.execute(
+        '''SELECT "resourceConfigId"
+           FROM "Resources"
+           WHERE id = %s;''',
+        (resourcesId,)
+        )
+        resource = cursor.fetchone()
+        if not resource:
+            raise ValueError(f"No resource found for resourcesId={resourcesId}")
+
+        resourceConfigId = resource[0]
+        
+        # Count VM OR DB OR ST instances
+        cursor.execute(
+            '''SELECT id FROM "AwsVMInstance" WHERE "resourceConfigId" = %s;''',
+            (resourceConfigId,)
+        )
+        vm_instances = cursor.fetchall()
+        vm_count = len(vm_instances)
+
+        cursor.execute(
+            '''SELECT id FROM "AwsDatabaseInstance" WHERE "resourceConfigId" = %s;''',
+            (resourceConfigId,)
+        )
+        db_instances = cursor.fetchall()
+        db_count = len(db_instances)
+
+        cursor.execute(
+            '''SELECT id FROM "AwsStorageInstance" WHERE "resourceConfigId" = %s;''',
+            (resourceConfigId,)
+        )
+        st_instances = cursor.fetchall()
+        st_count = len(st_instances)
+
+        cursor.close()
+        connection.close()
+
+        branches = []
+        if vm_count > 0:
+            branches.append('terraform_destroy_ec2')
+        if db_count > 0:
+            branches.append('terraform_destroy_rds')
+        if st_count > 0:
+            branches.append('terraform_destroy_s3')
+        if not branches:
+            return 'end'
+    finally:
+        cursor.close()
+        connection.close()
+    return branches
 # -------------------------
 # DAG Definition
 # -------------------------
 with DAG(
-    dag_id='AZURE_Destroy',
+    dag_id='AWS_Destroy',
     default_args=default_args,
     schedule=None,
     catchup=False,
@@ -305,7 +279,51 @@ with DAG(
         op_args=["{{ ti.xcom_pull(task_ids='get_request_id') }}"],
     )
 
-    # Branch based on resource types
+    destroy_rds = BashOperator(
+        task_id="terraform_destroy_rds",
+        bash_command=(
+            'cd "/opt/airflow/dags/terraform/{{ ti.xcom_pull(task_ids=\'get_repository_name\') | trim | replace(\'"\',\'\') }}/db" && '
+            'terraform init && terraform destroy -auto-approve'
+        ),
+        env={
+            "AWS_ACCESS_KEY_ID": os.getenv('AWS_ACCESS_KEY'),
+            "AWS_SECRET_ACCESS_KEY": os.getenv('AWS_SECRET_KEY'),
+            "AWS_DEFAULT_REGION": os.getenv('AWS_DEFAULT_REGION'),
+        },
+        retries=3,
+        retry_delay=timedelta(minutes=5)
+    )
+
+    destroy_s3 = BashOperator(
+        task_id="terraform_destroy_s3",
+        bash_command=(
+            'cd "/opt/airflow/dags/terraform/{{ ti.xcom_pull(task_ids=\'get_repository_name\') | trim | replace(\'"\',\'\') }}/st" && '
+            'terraform init && terraform destroy -auto-approve'
+        ),
+        env={
+            "AWS_ACCESS_KEY_ID": os.getenv('AWS_ACCESS_KEY'),
+            "AWS_SECRET_ACCESS_KEY": os.getenv('AWS_SECRET_KEY'),
+            "AWS_DEFAULT_REGION": os.getenv('AWS_DEFAULT_REGION'),
+        },
+        retries=3,
+        retry_delay=timedelta(minutes=5)
+    )
+
+    destroy_ec2 = BashOperator(
+        task_id="terraform_destroy_ec2",
+        bash_command=(
+            'cd "/opt/airflow/dags/terraform/{{ ti.xcom_pull(task_ids=\'get_repository_name\') | trim | replace(\'"\',\'\') }}/vm" && '
+            'terraform init && terraform destroy -auto-approve'
+        ),
+        env={
+            "AWS_ACCESS_KEY_ID": os.getenv('AWS_ACCESS_KEY'),
+            "AWS_SECRET_ACCESS_KEY": os.getenv('AWS_SECRET_KEY'),
+            "AWS_DEFAULT_REGION": os.getenv('AWS_DEFAULT_REGION'),
+        },
+        retries=3,
+        retry_delay=timedelta(minutes=5)
+    )
+
     branch_task = BranchPythonOperator(
         task_id='branch_resources',
         python_callable=branch_resources,
@@ -313,64 +331,6 @@ with DAG(
         retries=3,
         retry_delay=timedelta(minutes=5)
     )
-
-    # Destroy Azure VMs (same as existing)
-    destroy_vm = BashOperator(
-        task_id="terraform_destroy_vm",
-        bash_command=(
-            'cd "/opt/airflow/dags/terraform/{{ ti.xcom_pull(task_ids=\'get_repository_name\') | trim | replace(\'"\',\'\') }}/vm" && '
-            'terraform init && terraform destroy -auto-approve'
-        ),
-        retries=3,
-        retry_delay=timedelta(minutes=5)
-    )
-
-    # Destroy Azure Databases (same as existing)
-    destroy_db = BashOperator(
-        task_id="terraform_destroy_db",
-        bash_command=(
-            'cd "/opt/airflow/dags/terraform/{{ ti.xcom_pull(task_ids=\'get_repository_name\') | trim | replace(\'"\',\'\') }}/db" && '
-            'terraform init && terraform destroy -auto-approve'
-        ),
-        retries=3,
-        retry_delay=timedelta(minutes=5)
-    )
-
-    # Destroy Azure Storage (same as existing)
-    destroy_st = BashOperator(
-        task_id="terraform_destroy_st",
-        bash_command=(
-            'cd "/opt/airflow/dags/terraform/{{ ti.xcom_pull(task_ids=\'get_repository_name\') | trim | replace(\'"\',\'\') }}/st" && '
-            'terraform init && terraform destroy -auto-approve'
-        ),
-        retries=3,
-        retry_delay=timedelta(minutes=5)
-    )
-
-    # Destroy AKS Clusters (mimicking VM/DB/Storage pattern)
-    destroy_k8s = BashOperator(
-        task_id="terraform_destroy_k8s",
-        bash_command=(
-            'if [ -d "/opt/airflow/dags/terraform/{{ ti.xcom_pull(task_ids=\'get_repository_name\') | trim | replace(\'"\',\'\') }}/k8s" ]; then '
-            'cd "/opt/airflow/dags/terraform/{{ ti.xcom_pull(task_ids=\'get_repository_name\') | trim | replace(\'"\',\'\') }}/k8s" && '
-            'terraform init && terraform destroy -auto-approve; '
-            'else echo "K8s directory not found, skipping destroy"; fi'
-        ),
-        retries=3,
-        retry_delay=timedelta(minutes=5)
-    )
-
-    # Destroy Azure Resource Group (must be last)
-    # destroy_rg = BashOperator(
-    #     task_id="terraform_destroy_rg",
-    #     bash_command=(
-    #         'cd "/opt/airflow/dags/terraform/{{ ti.xcom_pull(task_ids=\'get_repository_name\') | trim | replace(\'"\',\'\') }}/resource_group" && '
-    #         'terraform init && terraform destroy -auto-approve'
-    #     ),
-    #     retries=3,
-    #     retry_delay=timedelta(minutes=5),
-    #     trigger_rule='none_failed_min_one_success'
-    # )
 
     # Cleanup folder
     cleanup_dir = PythonOperator(
@@ -390,11 +350,4 @@ with DAG(
         retry_delay=timedelta(minutes=5)
     )
 
-    # Task dependencies
-    get_request_id >> get_repository_name >> branch_task
-    branch_task >> [destroy_vm, destroy_db, destroy_st, destroy_k8s] >> cleanup_dir >> delete_request
-
-
-
-
-
+    get_request_id >> get_repository_name >> branch_task>> [destroy_ec2, destroy_rds, destroy_s3] >> cleanup_dir >> delete_request
