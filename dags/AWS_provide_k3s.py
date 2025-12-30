028d1d9308ff8ad0aa4f8273e54b113be6cccc8f
Thu Yein <thuyein1996alpha@gmail.com>
2025-12-30 18:05:30 +0700

    change config and nodeport

--- dags/AWS_provide_k3s.py
+++ dags/AWS_provide_k3s.py
@@ -189,18 +189,36 @@ resource "aws_security_group" "k3s_sg" {{
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.edge_sg.id]
   }}
   ingress {{
     from_port   = 6443
     to_port     = 6443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"]
+  }}
+  ingress {{
+    from_port   = 30080
+    to_port     = 30080
+    protocol    = "tcp"
+    security_groups = [aws_security_group.edge_sg.id]
+  }}
+  ingress {{
+    from_port   = 30443
+    to_port     = 30443
+    protocol    = "tcp"
+    security_groups = [aws_security_group.edge_sg.id]
   }}
   ingress {{
     from_port   = 0
     to_port     = 65535
     protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }}
+  ingress {{
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "udp"
     cidr_blocks = ["10.0.0.0/16"]
   }}
   egress {{
@@ -233,7 +251,7 @@ resource "aws_security_group" "edge_sg" {{
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }}
+  }}  
   egress {{
     from_port   = 0
     to_port     = 0
